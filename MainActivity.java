5e134db1a942368a8962e96771106c9fdc88c9a6
Anusree T K <94076173+anusreeanilanu@users.noreply.github.com>
2022-09-02 10:34:00 +0530

    lifecycle

--- MainActivity.java
+++ MainActivity.java
@@ -1,104 +1,53 @@
-package com.example.gridcalc;
+package com.example.lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String oldNumber="";
-    String op="+";
-    boolean isNewOp=true;
-    EditText ed1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ed1=findViewById(R.id.ed1h);
     }
-    public void NumberEvent(View view)
-    {
-        if(isNewOp)
-       
-            ed1.setText("");
-            isNewOp=false;
-            String number=ed1.getText().toString();
-            switch (view.getId())
-            {
-                case R.id.bu1:
-                    number+="1";
-                    break;
-                case R.id.bu2:
-                    number+="2";
-                    break;
-                case R.id.bu3:
-                    number+="3";
-                    break;
-                case R.id.bu4:
-                    number+="4";
-                    break;
-                case R.id.bu5:
-                    number+="5";
-                    break;
-                case R.id.bu6:
-                    number+="1";
-                    break;
-                case R.id.bu7:
-                    number+="7";
-                    break;
-                case R.id.bu8:
-                    number+="8";
-                    break;
-                case R.id.bu9:
-                    number+="9";
-                    break;
-                case R.id.bu0:
-                    number+="0";
-                    break;
-                case R.id.bu1clr:
-                    number="";
-                    break;
-            }
-            ed1.setText(number);
-    }
-    public void OperatorEvent(View view)
-    {
-      isNewOp=true;
-      String oldNumber=ed1.getText().toString();
-      switch (view.getId())
-        {
-            case R.id.buplus:
-                op="+";
-                break;
-            case R.id.buminus:
-                op="-";
-                break;
-            case R.id.bumul:
-                op="*";
-                break;
-            case R.id.budivide:
-                op="/";
-                break;
 
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "start...", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle", "onStart invoked");
     }
-    public void EqualEvent(View view)
-    {
-        String newNumber=ed1.getText().toString();
-        double result1=0.0;
-        switch (op)
-        {
-            case "+":result1=Double.parseDouble(oldNumber)+Double.parseDouble(newNumber);
-                    break;
-            case "-":result1=Double.parseDouble(oldNumber)-Double.parseDouble(newNumber);
-                break;
-            case "*":result1=Double.parseDouble(oldNumber)*Double.parseDouble(newNumber);
-                break;
-            case "/":result1=Double.parseDouble(oldNumber)/Double.parseDouble(newNumber);
-                break;
-        }
-        ed1.setText(result1+"");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Resume...", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Pause...", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Stop...", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Restart..", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Destroy..", Toast.LENGTH_SHORT).show();
+        Log.d("lifecycle","onDestroy invoked");
     }
 }
